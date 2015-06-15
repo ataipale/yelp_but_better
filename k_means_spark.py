5c44f19f0c7c09f464bdb4bdb26f8266c9f8aca6
Alex Taipale <alextaipale@gmail.com>
2015-06-15 18:22:46 -0400

    updating json loading...

--- k_means_spark.py
+++ k_means_spark.py
@@ -1,3 +1,5 @@
+# run in ipython spark shell, IPYTHON=1 pyspark
+
 from pyspark import SparkContext
 from pyspark.mllib.feature import HashingTF
 from pyspark.mllib.clustering import KMeans, KMeansModel
@@ -5,13 +7,13 @@ from numpy import array
 from math import sqrt
 import json
 
-def load(f):
-    try:
-        json.loads(f)
-    except:
-        pass
-        
-documents = sc.textFile("sample_tweets2.json").map(lambda line: load(line))
+# def load(f):
+#     try:
+#         json.loads(f)
+#     except:
+#         pass
+
+documents = sc.textFile("sample_tweets2.json").map(lambda line: json.loads(line))
 hashingTF = HashingTF()
 tf = hashingTF.transform(documents)
 clusters = KMeans.train(tf, 2, maxIterations=10, runs=10, initializationMode="random")
