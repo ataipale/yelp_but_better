a0c6e23da15e4ed9862e08f00f62800e67c13541
Alex Taipale <alextaipale@gmail.com>
2015-06-15 18:39:09 -0400

    updating sample doc

--- k_means_spark.py
+++ k_means_spark.py
@@ -7,13 +7,13 @@ from numpy import array
 from math import sqrt
 import json
 
-# def load(f):
-#     try:
-#         json.loads(f)
-#     except:
-#         pass
 
-documents = sc.textFile("sample_tweets2.json").map(lambda line: json.loads(line))
+
+documents = sc.textFile("sample.json").map(lambda line: json.loads(line))
 hashingTF = HashingTF()
 tf = hashingTF.transform(documents)
 clusters = KMeans.train(tf, 2, maxIterations=10, runs=10, initializationMode="random")
+
+
+
+            
