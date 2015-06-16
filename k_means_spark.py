6ef5696e3ac2a5443e0d719bc2fa8a2e16b1d83b
Alex Taipale <alextaipale@gmail.com>
2015-06-16 16:51:47 -0400

    updating TFIDF input, still memory heap problems

--- k_means_spark.py
+++ k_means_spark.py
@@ -6,14 +6,19 @@ from pyspark.mllib.clustering import KMeans, KMeansModel
 from numpy import array
 from math import sqrt
 import json
+from pyspark.sql import SQLContext, Row
 
 
+sqlContext = SQLContext(sc)
+sample = sqlContext.read.json("./sample.json")
+sample = sqlContext.read.json("/home/ubuntu/yelp_project/sample.json")
+sample.registerTempTable("sample")
+reviews = sample.map(lambda x: Row(name= x[1], reviews=' '.join((a[3] for a in x[0])))) 
 
-documents = sc.textFile("sample.json").map(lambda line: json.loads(line))
+# documents = sc.textFile("sample.json", 10).map(lambda line: json.loads(line))
 hashingTF = HashingTF()
-tf = hashingTF.transform(documents)
+tf = hashingTF.transform(reviews.map(lambda x: x.reviews))
 clusters = KMeans.train(tf, 2, maxIterations=10, runs=10, initializationMode="random")
 
-
-
-            
+# sqlContext.sql("SELECT lalal FROM sample")
+#give list of lists, each line is all reviews of restaurant
