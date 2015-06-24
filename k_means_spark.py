c5c54c1b2eb6ba0338c9038d8a070e9fc5ee0ebf
Alex Taipale <alextaipale@gmail.com>
2015-06-24 13:00:59 -0400

    updating cleaning of reviews

--- k_means_spark.py
+++ k_means_spark.py
@@ -1,5 +1,9 @@
 # run in ipython spark shell, IPYTHON=1 pyspark
 
+# When I was trying to edit memory use I edited these files:
+# ~/spark-1.4.0-bin-hadoop2.6/conf
+# vi spark-env.sh
+
 from pyspark import SparkContext
 from pyspark.mllib.feature import HashingTF
 from pyspark.mllib.clustering import KMeans, KMeansModel
@@ -7,18 +11,26 @@ from numpy import array
 from math import sqrt
 import json
 from pyspark.sql import SQLContext, Row
+from pyspark.mllib.feature import IDF
 
 
+stopwords = [i.strip() for i in open("stopwords_en.txt")]
 sqlContext = SQLContext(sc)
-sample = sqlContext.read.json("./sample.json")
 sample = sqlContext.read.json("/home/ubuntu/yelp_project/sample.json")
 sample.registerTempTable("sample")
-reviews = sample.map(lambda x: Row(name= x[1], reviews=' '.join((a[3] for a in x[0])))) 
+# take only the reviews from each row of the sample
+reviews = sample.map(lambda x: Row(name= x.name, reviews=' '.join(a[3] for a in x.reviews)))
+reviews_clean = reviews.map(lambda x: Row(name = x.name, reviews = ' '.join(word for word in x.reviews if word.lower() not in stopwords)))
+# sample = sqlContext.read.json("./sample.json")
 
 # documents = sc.textFile("sample.json", 10).map(lambda line: json.loads(line))
-hashingTF = HashingTF()
-tf = hashingTF.transform(reviews.map(lambda x: x.reviews))
-clusters = KMeans.train(tf, 2, maxIterations=10, runs=10, initializationMode="random")
+#initialize with max number of features you want in your TFIDF
+hashingTF = HashingTF(5000) 
+tf = hashingTF.transform(reviews_clean.map(lambda x: x.reviews))
+tf.cache()
+idf = IDF(minDocFreq=2).fit(tf)
+tfidf = idf.transform(tf)
+clusters = KMeans.train(tfidf, 2, maxIterations=10, runs=10, initializationMode="random")
 
 # sqlContext.sql("SELECT lalal FROM sample")
 #give list of lists, each line is all reviews of restaurant
